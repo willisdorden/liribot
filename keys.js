0f674f766d2dea071365fbfa53442e947efe84a7
willisdorden <willisdorden@gmail.com>
2017-03-30 19:39:25 -0400

    working twitter and omdb

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 // twitter api
 
 
@@ -8,4 +8,10 @@ exports.twitterKeys = {
     consumerSecret: 'jEz8bVqqadfBHQHCNzF0KPPrKiLhZuql14i5Lqo5buyQ6e1Ezo',
     accessTokenKey: '3397179519-3ZX156mpPQk0bDblLUojRI2jWFbCWSVZKX8Yy2n',
     accessTokenSecret: 'PUfoXSwbs8sq8T1hu3fpMLHRtKLzsYZ1DqaGlVKxswXay',
+};
+
+exports.spotifyKeys ={
+    clientId: 'c6a7caccd64141bd8f1efb4e70c2550e',
+    clientSecret: 'd62926ac112643389912c1c6c89f531e',
+    redirectUri: 'https://www.example.com/callback'
 };
--- liri.js
+++ liri.js
@@ -1,17 +1,94 @@
 var keys = require('./keys.js');
 var Twitter = require('twitter');
+var request = require('request');
+var SpotifyWebApi = require('spotify-web-api-node');
 
-var client = new Twitter({
-    consumer_key: keys.twitterKeys.consumerKey,
-    consumer_secret: keys.twitterKeys.consumerSecret,
-    access_token_key: keys.twitterKeys.accessTokenKey,
-    access_token_secret: keys.twitterKeys.accessTokenSecret
-});
-var params = { screen_name: 'laravelphp', count: 20 };
+// the twitter function
+function myTweets() {
+    // this is grabbing my keys from keys.js
+    var client = new Twitter({
+        consumer_key: keys.twitterKeys.consumerKey,
+        consumer_secret: keys.twitterKeys.consumerSecret,
+        access_token_key: keys.twitterKeys.accessTokenKey,
+        access_token_secret: keys.twitterKeys.accessTokenSecret
+    });
+// this is setting params to the screenname and setting a limit
+    var params = {screen_name: 'laravelphp', count: 20};
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            console.log("finish");
+            for (var i = 0;i < tweets.length;i++) {
+                console.log(tweets[i].text);
+            }
+        }
+    });
+}
+// the omdb function
+function movie() {
 
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        console.log("finish");
-        console.log(tweets);
-    }
-});
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            var body = JSON.parse(body);
+           // console.log("The movie's rating is: " + body.imdbRating);
+           // console.log(body);
+            console.log(body.Title);
+            console.log(body.Year) ;
+            console.log(body.Rated) ;
+            console.log(body.Country);
+            console.log(body.Language);
+            console.log(body.Plot);
+            console.log(body.Actors);
+            console.log(body.Ratings[1]);
+            //console.log(queryUrl);
+        }
+    });
+}
+
+function songs() {
+
+    var client = new SpotifyWebApi({
+        clientId: keys.spotifyKeys.clientId,
+        clientSecret: keys.spotifyKeys.clientSecret,
+        redirectUri: keys.spotifyKeys.redirectUri,
+    });
+    spotify.search({type: 'track', query:" dancing in the moonlight "}, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+
+        }
+        console.log(data.items);
+    });
+}
+// im grabbing the userinput and calling the function
+var userinput = process.argv[2];
+//console.log(userinput);
+//console.log(movieName);
+var movieName = process.argv[3];
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "=&plot=short&r=json";
+var trackName = process.argv[3];
+
+    //if(userinput === "myTweets"){
+      //  myTweets();
+   // }if (userinput === "Movie-this"){
+     //   movie(movieName);
+    //}
+switch (userinput){
+    case "myTweets":
+        myTweets();
+        break;
+
+    case "Movie-this":
+        movie(queryUrl);
+        break;
+
+    case "spotify-this-song":
+        songs();
+        break;
+}
+
